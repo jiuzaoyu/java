566640226df9d28c4772ad3b40880ca4d1b7d0e4
jiuzaoyu <765248848@qq.com>
2017-12-15 17:05:21 +0800

    20171215

---
+++ java基础/去掉string[]中重复的元素.java
@@ -0,0 +1,39 @@
+package com.changyou.erp.common;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+
+public class CopyOfStringUtil {
+   
+	/**
+	 * 把String[]去重
+	 * @param arr
+	 * @return
+	 */
+	public static void unique(String [] arr){
+        //实例化一个set集合
+        Set<String> set = new HashSet<String>();
+        if(arr == null){
+        	return;
+        }
+        //遍历数组并存入集合,如果元素已存在则不会重复存入
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+            arr[i] = "";
+        }
+        
+        int j = 0;
+        for(String str : set){
+        	arr[j] = str;
+        	j++;
+        }
+    } 
+}
